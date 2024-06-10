e063ce22c16777928218f0475f1a668c1c894425
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-10 15:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+    var textNode = document.createTextNode("DOM load success"); 
+    document.body.appendChild(textNode);
+})
